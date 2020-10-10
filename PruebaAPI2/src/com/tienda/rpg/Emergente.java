a6cc54a2eaa94c4d9f25d5fe909f67fba7406896
Valhylian <71191912+Valhylian@users.noreply.github.com>
2020-10-10 00:28:11 -0600

    Preview de stats de cada item

--- PruebaAPI2/src/com/tienda/rpg/Emergente.java
+++ PruebaAPI2/src/com/tienda/rpg/Emergente.java
@@ -73,6 +73,7 @@ public static JPanel contentPane;
 		lblNewLabel_3.setBounds(320, 18, 75, 14);
 		contentPane.add(lblNewLabel_3);
 		
+		/*
 		JLabel lblNewLabel_4 = new JLabel("Stats:");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblNewLabel_4.setBounds(10, 113, 46, 14);
@@ -115,6 +116,132 @@ public static JPanel contentPane;
 			lblNewLabel_8.setBounds(89, 192, 46, 14);
 			contentPane.add(lblNewLabel_8);
 		}
+		*/
+		
+		if (categoria == "Armadura") {
+			JLabel lblStat1 = new JLabel("DESTREZA");
+			lblStat1.setBounds(10, 194, 71, 14);
+			contentPane.add(lblStat1);
+			
+			JLabel lblStat2 = new JLabel("RESISTENCIA");
+			lblStat2.setBounds(10, 219, 71, 14);
+			contentPane.add(lblStat2);
+		}
+		
+		else if (categoria == "Arma") {
+			JLabel lblStat1 = new JLabel("FUERZA");
+			lblStat1.setBounds(10, 194, 71, 14);
+			contentPane.add(lblStat1);
+			
+			JLabel lblStat2 = new JLabel("DESTREZA");
+			lblStat2.setBounds(10, 219, 71, 14);
+			contentPane.add(lblStat2);
+		}
+		
+		else if (categoria == "Pocima") {
+			JLabel lblStat1 = new JLabel("SABIDURIA");
+			lblStat1.setBounds(10, 194, 71, 14);
+			contentPane.add(lblStat1);
+			
+			JLabel lblStat2 = new JLabel("CARISMA");
+			lblStat2.setBounds(10, 219, 71, 14);
+			contentPane.add(lblStat2);
+		}
+		
+		//STATS ACTUALES
+		if (categoria == "Armadura") {
+			JLabel lblAntes1 = new JLabel(Integer.toString(Personaje.destreza));
+			lblAntes1.setBounds(117, 194, 46, 14);
+			contentPane.add(lblAntes1);
+			
+			JLabel lblAntes2 = new JLabel(Integer.toString(Personaje.resistencia));
+			lblAntes2.setBounds(117, 219, 46, 14);
+			contentPane.add(lblAntes2);	
+		}
+		
+		else if (categoria == "Arma") {
+			JLabel lblAntes1 = new JLabel(Integer.toString(Personaje.fuerza));
+			lblAntes1.setBounds(117, 194, 46, 14);
+			contentPane.add(lblAntes1);
+			
+			JLabel lblAntes2 = new JLabel(Integer.toString(Personaje.destreza));
+			lblAntes2.setBounds(117, 219, 46, 14);
+			contentPane.add(lblAntes2);
+		}
+		
+		else if (categoria == "Pocima") {
+			JLabel lblAntes1 = new JLabel(Integer.toString(Personaje.sabiduria));
+			lblAntes1.setBounds(117, 194, 46, 14);
+			contentPane.add(lblAntes1);
+			
+			JLabel lblAntes2 = new JLabel(Integer.toString(Personaje.carisma));
+			lblAntes2.setBounds(117, 219, 46, 14);
+			contentPane.add(lblAntes2);
+		}
+		
+		//MOD PUNTOS DE STATS
+		if (categoria == "Armadura") {
+			JLabel lblMas1 = new JLabel(Double.toString(Item.modDestreza(json)));
+			lblMas1.setBounds(208, 194, 46, 14);
+			contentPane.add(lblMas1);
+			
+			JLabel lblMas2 = new JLabel(Integer.toString(Item.modResistencia(json)));
+			lblMas2.setBounds(208, 219, 46, 14);
+			contentPane.add(lblMas2);
+		}
+		
+		else if (categoria == "Arma") {
+			JLabel lblMas1 = new JLabel(Double.toString(Item.modFuerza(json)));
+			lblMas1.setBounds(208, 194, 46, 14);
+			contentPane.add(lblMas1);
+			
+			JLabel lblMas2 = new JLabel(Double.toString(Item.modDestreza(json)));
+			lblMas2.setBounds(208, 219, 46, 14);
+			contentPane.add(lblMas2);
+		}
+		
+		else if (categoria == "Pocima") {
+			JLabel lblMas1 = new JLabel(Double.toString(Item.modSabiduria(json)));
+			lblMas1.setBounds(208, 194, 46, 14);
+			contentPane.add(lblMas1);
+			
+			JLabel lblMas2 = new JLabel(Double.toString(Item.modCarisma(json)));
+			lblMas2.setBounds(208, 219, 46, 14);
+			contentPane.add(lblMas2);
+		}
+		
+		//SUMA (AFECTACION DE STATS)
+		if (categoria == "Armadura") {
+			JLabel lblDesp1 = new JLabel(Double.toString(Item.modDestreza(json)+Personaje.destreza));
+			lblDesp1.setBounds(299, 194, 46, 14);
+			contentPane.add(lblDesp1);
+			
+			JLabel lblDesp2 = new JLabel(Integer.toString(Item.modResistencia(json)+Personaje.resistencia));
+			lblDesp2.setBounds(299, 219, 46, 14);
+			contentPane.add(lblDesp2);
+		}
+		
+		else if (categoria == "Arma") {
+			JLabel lblDesp1 = new JLabel(Double.toString(Item.modFuerza(json)+Personaje.fuerza));
+			lblDesp1.setBounds(299, 194, 46, 14);
+			contentPane.add(lblDesp1);
+			
+			JLabel lblDesp2 = new JLabel(Double.toString(Item.modDestreza(json)+Personaje.destreza));
+			lblDesp2.setBounds(299, 219, 46, 14);
+			contentPane.add(lblDesp2);
+		}
+		
+		else if (categoria == "Pocima") {
+			JLabel lblDesp1 = new JLabel(Double.toString(Item.modSabiduria(json)+Personaje.sabiduria));
+			lblDesp1.setBounds(299, 194, 46, 14);
+			contentPane.add(lblDesp1);
+			
+			JLabel lblDesp2 = new JLabel(Double.toString(Item.modCarisma(json)+Personaje.carisma));
+			lblDesp2.setBounds(299, 219, 46, 14);
+			contentPane.add(lblDesp2);
+
+		}
+		
 	}
 	
 	/*
@@ -130,6 +257,32 @@ public static JPanel contentPane;
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		JLabel lblModStatsTit = new JLabel("MODIFICACION DE STATS:");
+		lblModStatsTit.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblModStatsTit.setBounds(10, 147, 153, 14);
+		contentPane.add(lblModStatsTit);
+		
+		JLabel lblStatTit = new JLabel("STAT:");
+		lblStatTit.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblStatTit.setBounds(10, 172, 46, 14);
+		contentPane.add(lblStatTit);
+		
+		JLabel lblAntesTit = new JLabel("ANTES:");
+		lblAntesTit.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblAntesTit.setBounds(117, 172, 46, 14);
+		contentPane.add(lblAntesTit);
+		
+		JLabel lblMasTit = new JLabel("    +");
+		lblMasTit.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblMasTit.setBounds(208, 172, 46, 14);
+		contentPane.add(lblMasTit);
+		
+		JLabel lblDespuesTit = new JLabel("DESPUES:");
+		lblDespuesTit.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDespuesTit.setBounds(299, 172, 79, 14);
+		contentPane.add(lblDespuesTit);
+		
+		
 		//labels ("");
 		
 	}
